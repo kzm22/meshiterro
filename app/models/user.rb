fd2b2494035462a1f9a0b3378e6e143a1db2b86f
EC2 Default User <ec2-user@ip-172-31-41-190.ap-northeast-1.compute.internal>
2023-02-01 13:58:56 +0000

    [Update]5-9

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+         
+  has_many :post_images, dependent: :destroy
 end
   #「パスワードの正確性を検証」、「ユーザ登録や編集、削除」、
   #「パスワードをリセット」、「ログイン情報を保存」、「emailのフォーマットなどのバリデーション」
