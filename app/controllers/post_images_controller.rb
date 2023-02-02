6375d5af12c2e27f224f6807546b8fc87e8c7177
EC2 Default User <ec2-user@ip-172-31-41-190.ap-northeast-1.compute.internal>
2023-02-02 14:19:38 +0000

    [update]5-11

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,10 +1,33 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
+  end
+  
+  def create
+    #投稿フォームで入力されたデータ（shop_name,image,caption)が@post_imageに格納される
+    @post_image = PostImage.new(post_image_params)
+    #投稿データのuser_idを今ログインしているユーザーIDにすることで投稿データに持たせることができる。
+    @post_image.user_id = current_user.id
+    #格納されたデータを保存する
+    @post_image.save
+    #post_images画面に戻る
+    redirect_to post_images_path
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
   end
+  
+  def destroy
+  end
+  
+  private
+  
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+  
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,4 +3,21 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   
+  def get_image #メソッド
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
+  
+  def get_image
+    #画像が設定されていない場合は、app/~に格納されているno_image.jpgという画像をデフォルト画像としてActivestrageに格納し、格納した画像を表示する
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image  
+  end
+  
 end
