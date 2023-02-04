4b85d4f6745f2fa692ebbb3ef4249f3e755200a1
EC2 Default User <ec2-user@ip-172-31-41-190.ap-northeast-1.compute.internal>
2023-02-04 08:35:22 +0000

    [Update]5-12

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,7 +3,7 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   
-  def get_image #メソッド
+  def get_image 
     if image.attached?
       image
     else
@@ -15,7 +15,7 @@ class PostImage < ApplicationRecord
     #画像が設定されていない場合は、app/~に格納されているno_image.jpgという画像をデフォルト画像としてActivestrageに格納し、格納した画像を表示する
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
     image  
   end
